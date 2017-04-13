fd3a9114c6bb7ff4fdf7f2f385d255f6b87459d2
Andrew Rock <Andrew Rock>
2017-04-13 12:50:41 -0500

    homepage javascript

--- js/homePage.js
+++ js/homePage.js
@@ -1,6 +1,25 @@
 
-var indexProducts = document.getElementById("indexProducts").addEventListener("click", window.location.href = "products.html");
+var indexProduct = document.getElementById("indexProduct");
 var indexMission = document.getElementById("indexMission");
 var indexTeam = document.getElementById("indexTeam");
 var indexContact = document.getElementById("indexContact");
 
+indexProduct.addEventListener("mousedown", moveProduct);
+indexMission.addEventListener("mousedown", moveMission);
+indexTeam.addEventListener("mousedown", moveTeam);
+indexContact.addEventListener("mousedown", moveContact);
+
+
+
+function moveProduct() {
+	document.location.href='products.html';
+}
+function moveMission() {
+	document.location.href='mission.html';
+}
+function moveTeam() {
+	document.location.href='team.html';
+}
+function moveContact() {
+	document.location.href='contact.html';
+}
